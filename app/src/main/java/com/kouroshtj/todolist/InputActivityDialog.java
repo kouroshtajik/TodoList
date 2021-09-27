2665293f50de06f771d2c1de287078ac236409dd
kouroshtj74 <43277649+kouroshtj74@users.noreply.github.com>
2021-09-27 10:10:23 +0330

    Recycler view

---
+++ app/src/main/java/com/kouroshtj/todolist/InputActivityDialog.java
@@ -0,0 +1,14 @@
+package com.kouroshtj.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputActivityDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_dialog);
+    }
+}
